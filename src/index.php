2b5136bb95461c48a47d42e26db5552296f841e7
Murphy Horta <murphy@2nv.co>
2021-04-21 11:53:59 -0500

    :lipstick: improving styles

--- src/index.php
+++ src/index.php
@@ -1,67 +1,68 @@
 <!DOCTYPE html>
 <?php
 if (
-    (isset($_POST['cliente'])) && ($_POST['cliente'] != '') && 
-    (isset($_POST['nombre'])) && ($_POST['nombre'] != '') && 
-    (isset($_POST['fecha_inicio'])) && ($_POST['fecha_inicio'] != '') && 
-    (isset($_POST['ppto_horas'])) && ($_POST['ppto_horas'] != '') && 
-    (isset($_POST['clave_id'])) && ($_POST['clave_id'] != '') && 
-    (isset($_POST['descripcion'])) && ($_POST['descripcion'] != '') && 
-    (isset($_POST['fecha_fin'])) && ($_POST['fecha_fin'] != '') && 
-    (isset($_POST['fecha_fin_real'])) && ($_POST['fecha_fin_real'] != '') && 
-    (isset($_POST['nom_contacto'])) && ($_POST['nom_contacto'] != '') && 
+    (isset($_POST['cliente'])) && ($_POST['cliente'] != '') &&
+    (isset($_POST['nombre'])) && ($_POST['nombre'] != '') &&
+    (isset($_POST['fecha_inicio'])) && ($_POST['fecha_inicio'] != '') &&
+    (isset($_POST['ppto_horas'])) && ($_POST['ppto_horas'] != '') &&
+    (isset($_POST['clave_id'])) && ($_POST['clave_id'] != '') &&
+    (isset($_POST['descripcion'])) && ($_POST['descripcion'] != '') &&
+    (isset($_POST['fecha_fin'])) && ($_POST['fecha_fin'] != '') &&
+    (isset($_POST['fecha_fin_real'])) && ($_POST['fecha_fin_real'] != '') &&
+    (isset($_POST['nom_contacto'])) && ($_POST['nom_contacto'] != '') &&
     (isset($_POST['equipo'])) && ($_POST['equipo'] != '')
-    ) {
+) {
 
     include "models/modelo.php";
     $nuevo = new Service();
     $datos = $nuevo->setServicio(
-            $_POST['cliente'], 
-            $_POST['nombre'], 
-            $_POST['fecha_inicio'], 
-            $_POST['ppto_horas'], 
-            $_POST['clave_id'], 
-            $_POST['descripcion'], 
-            $_POST['fecha_fin'], 
-            $_POST['fecha_fin_real'], 
-            $_POST['nom_contacto'], 
-            $_POST['equipo']
+        $_POST['cliente'],
+        $_POST['nombre'],
+        $_POST['fecha_inicio'],
+        $_POST['ppto_horas'],
+        $_POST['clave_id'],
+        $_POST['descripcion'],
+        $_POST['fecha_fin'],
+        $_POST['fecha_fin_real'],
+        $_POST['nom_contacto'],
+        $_POST['equipo']
     );
 }
 ?>
 
 
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Ejemplo MVC con PHP</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-        <style>
-            .abs-center {
+
+<head>
+    <meta charset="UTF-8">
+    <title>Ejemplo MVC con PHP</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <style>
+        .abs-center {
             display: flex;
             align-items: center;
             justify-content: center;
             min-height: 100vh;
-            }
+        }
 
-            .form {
+        .form {
             width: 650px;
-            }
-        </style>
-    </head>
-    <body>
+        }
+    </style>
+</head>
+
+<body>
     <div class="container">
-        <div class="abs-center">              
+        <div class="abs-center">
             <div class="row">
                 <div class="col-sm-10">
                     <nav aria-label="breadcrumb">
-                      <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="#">Inicio</a></li>
-                        <li class="breadcrumb-item"><a href="controllers/controlador.php">Ver servicios</a></li>
-                      </ol>
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="#">Inicio</a></li>
+                            <li class="breadcrumb-item"><a href="controllers/controlador.php">Ver servicios</a></li>
+                        </ol>
                     </nav>
                     <form class="border p-3 form" autocomplete="off" name="formulario" method="POST">
 
@@ -82,7 +83,7 @@ if (
                                 </div>
                                 <div class="form-group">
                                     <label for="validarNombre">Ppto horas:<span class="red">*</span></label>
-                                    
+
                                     <div class="input-group">
                                         <div class="input-group-prepend"><span class="input-group-text">T</span></div>
                                         <input type="number" class="form-control" id="ppto_horas" name="ppto_horas" tabindex="7" required="">
@@ -105,11 +106,11 @@ if (
                                 <div class="form-group">
                                     <label for="validarNombre">Fecha Fin (real):<span class="red">*</span></label>
                                     <input type="date" class="form-control" id="fecha_fin_real" name="fecha_fin_real" tabindex="8" required="">
-                                </div> 
+                                </div>
                             </div>
                         </div>
-            
-                        <h1><small>Datos de contacto</small></h1>            
+
+                        <h1><small>Datos de contacto</small></h1>
                         <div class="row">
                             <div class="col-sm-6">
                                 <div class="form-group">
@@ -129,14 +130,21 @@ if (
                                 </div>
                             </div>
                         </div>
-            
+
                         <div class="error" id="error"></div>
-            
+
                         <button class="btn btn-primary" type="submit"> Enviar </button>
-                      </form>    
+                    </form>
                 </div>
-            </div>              
-        </div>        
-    </div>     
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous">
+    </script>
 </body>
+
 </html>
--- src/views/vista.php
+++ src/views/vista.php
@@ -4,10 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <title>Ejemplo MVC con PHP</title>
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 </head>
 
 <body>
@@ -16,22 +14,26 @@
             <h1>Registro de Servicios</h1>
         </header>
         <div class="col-lg-12 text-center">
+
+
             <table class="table">
-                <thead class="table-light">
+                <thead>
                     <tr>
-                        <td><strong>Servicio</strong></td>
-                        <td><strong>Cliente</strong></td>
-                        <td><strong>Id Cliente</strong></td>
-                        <td><strong>Descripción</strong></td>
-                        <td><strong>Fecha inicio</strong></td>
-                        <td><strong>Fecha fin (planeada)</strong></td>
-                        <td><strong>Ppto horas</strong></td>
-                        <td><strong>Fecha fin (real)</strong></td>
-                        <td><strong>Nom contacto</strong></td>
-                        <td><strong>Equipo de trabajo</strong></td>
+                        <th scope="col"> Servicio </th>
+                        <th scope="col"> Cliente </th>
+                        <th scope="col"> Id Cliente </th>
+                        <th scope="col"> Descripción </th>
+                        <th scope="col"> Fecha inicio </th>
+                        <th scope="col"> Fecha fin (planeada) </th>
+                        <th scope="col"> Ppto horas </th>
+                        <th scope="col"> Fecha fin (real) </th>
+                        <th scope="col"> Nom contacto </th>
+                        <th scope="col"> Equipo de trabajo </th>
                         <td></td>
                         <td></td>
                     </tr>
+                </thead>
+                <tbody>
                     <?php
                     for ($i = 0; $i < count($datos); $i++) {
                     ?>
@@ -53,11 +55,20 @@
                     <?php
                     }
                     ?>
+                </tbody>
             </table>
+
+
             <a href="../index.php"> <i class="fa fa-arrow-circle-left"></i> Volver a la página principal</a>
             <hr />
         </div>
     </div>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
